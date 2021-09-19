dbab43157f7d7dc6080ad7d7fa9efc80f0bff26c
Gustavo Hurtado Cristia <gustavo.hurtado@coresolutions.es>
2021-09-19 06:37:53 +0200

    Actualizando TOKEN

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  mapboxToken:'TOKEN-MAPBOX'
+  mapboxToken:'pk.eyJ1IjoiZ2hjY3Jpc3RpYSIsImEiOiJja3RqcWx0ODAxZWdlMzJxZmw3MThsZ3B1In0.Q9qt5wsgg-n4WhCbmHbPTw'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@
 
 export const environment = {
   production: false,
-  mapboxToken:'TOKEN-MAPBOX'
+  mapboxToken:'pk.eyJ1IjoiZ2hjY3Jpc3RpYSIsImEiOiJja3RqcWx0ODAxZWdlMzJxZmw3MThsZ3B1In0.Q9qt5wsgg-n4WhCbmHbPTw'
 };
