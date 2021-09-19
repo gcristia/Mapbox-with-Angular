585bc0b2ac9de5565c44999fb5c2baa7a4bd403c
Gustavo Hurtado Cristia <gustavo.hurtado@coresolutions.es>
2021-09-19 06:36:48 +0200

    Eliminando TOKEN

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  mapboxToken:'pk.eyJ1IjoiZ2hjY3Jpc3RpYSIsImEiOiJja3RqcWx0ODAxZWdlMzJxZmw3MThsZ3B1In0.Q9qt5wsgg-n4WhCbmHbPTw'
+  mapboxToken:'TOKEN-MAPBOX'
 };
